d5d4cabdf18c951970a9911d3c4c3485ecde913b
neuro <nanenaro@gmail.com>
2021-08-11 17:32:08 +0300

    unit written

--- stat/binary_unit_stat.py
+++ stat/binary_unit_stat.py
@@ -10,9 +10,14 @@ class BinaryUnitStat:
             self.pics = get_diverse_set_of_numbers(100)
 
     def get_unconditional_sample(self, sample_size=300):
+        # в случайных точках случайгых картинок проводим замер этим юнитом,
+        # результат (ноль или один) записываем в выборку, возвращаем ее
         pass
 
-    def get_conditional_sample(self):
+    def get_conditional_sample(self, condition):
+        # пробегаем по всем картинкам, в каждой точке проверяя condition
+        # в тех точках, где condition=True, провоим замер этим юнитом,
+        # результат (ноль или один) записываем в выборку, возвращаем ее
         pass
 
 
--- unit.py
+++ unit.py
@@ -1,3 +1,5 @@
+from sensors import *
+
 class BinaryUnit:
     def __init__(self, u_radius, sensor_field_radius, etalon, event_diameter, dx,dy):
         self.u_radius = u_radius
@@ -7,8 +9,17 @@ class BinaryUnit:
         self.dx = dx
         self.dy = dy
 
-    def apply(self,pic, x,y): # returns 1 or 0
-        pass
+    def apply(self, pic, x,y):  # retuurns list of coords of all 1-s
+        expected_x = x + self.dx
+        expected_y = y + self.dy
+        matches = []
+        for r in range(0, self.u_radius + 1):
+            X, Y = get_coords_for_radius(expected_x, expected_y, r)
+            for i in range(len(X)):
+                mean = make_measurement(pic, X[i], Y[i], self.sensor_field_radius)
+                if mean >= self.event_detector_min and mean <= self.event_detector_max:
+                    matches.append([X[i],Y[i]])
+        return matches
 
 
 class NonBinaryUnit:
@@ -20,4 +31,10 @@ class NonBinaryUnit:
         self.dy = dy
 
     def apply(self, pic, x, y): # returns float number
-        pass
+        X, Y = get_coords_less_or_eq_raduis(x + self.dx, y + self.dy, self.u_radius)
+        nearest_mean = make_measurement(pic, X[0], Y[0], self.sensor_field_radius)
+        for i in range(1, len(X)):
+            mean = make_measurement(pic, X[i], Y[i], self.sensor_field_radius)
+            if abs(mean - self.etalon) < abs(nearest_mean - self.etalon):
+                nearest_mean = mean
+        return nearest_mean
