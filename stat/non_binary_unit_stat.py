0d50fec0cf9b6b079a1b423cb77c0c5ce1aa54f3
neuro <nanenaro@gmail.com>
2021-08-11 17:56:45 +0300

    NonBinaryUnitStat

--- stat/non_binary_unit_stat.py
+++ stat/non_binary_unit_stat.py
@@ -1,8 +1,9 @@
 from stat.basic_stat import SensRadiusesEmpiricDatabase
 from unit import NonBinaryUnit
 from data import *
+from utils import *
 
-class BinaryUnitStat:
+class NonBinaryUnitStat:
     def __init__(self, nonbinary_unit, pics=None):
         self.nonbinary_unit=nonbinary_unit
         self.pics = pics
@@ -12,10 +13,29 @@ class BinaryUnitStat:
     def get_unconditional_sample(self, sample_size=300):
         # в случайных точках случайгых картинок проводим замер этим юнитом,
         # результат (число) записываем в выборку, возвращаем ее
-        pass
+        activations = []
+        while True:
+            if len(activations) == sample_size:
+                return activations
+            pic = select_random_pic(self.pics)
+            x,y = select_random_xoord_on_pic(pic)
+            activation = self.nonbinary_unit.apply(pic, x, y)
+            activations.append(activation)
 
-    def get_conditional_sample(self):
+
+    def get_conditional_sample(self, condition):
         # пробегаем по всем картинкам, в каждой точке проверяя condition
         # в тех точках, где condition=True, провоим замер этим юнитом,
         # результат (число) записываем в выборку, возвращаем ее
-        pass
+        ymax = self.pics[0].shape[0]
+        xmax = self.pics[0].shape[1]
+        activations = []
+        for pic in self.pics:
+            for y in range(0, ymax):
+                for x in range(0, xmax):
+                    if condition(pic, x, y):
+                        activation = self.nonbinary_unit.apply(pic, x,y)
+                        activations.append(activation)
+        return activations
+
+
---
+++ utils.py
@@ -0,0 +1,12 @@
+from random import choice
+import random
+
+def select_random_pic(pics):
+    return choice(pics)
+
+def select_random_xoord_on_pic(pic):
+    maxX = pic.shape[1]
+    maxY = pic.shape[0]
+    x = random.randint(0, maxX - 1)
+    y = random.randint(0, maxY - 1)
+    return x,y
